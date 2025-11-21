d22b66e9ed97a7ee6206eaa9b8feb398134a6d3e
jw <yunjwon94@gmail.com>
2025-11-21 12:33:48 +0900

    feat: 저장된 파일 목록 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+interface IFile {
+  id: string;
+  name: string;
+  key: string;
+  url: string;
+  createdAt: string;
+}
 
 function App() {
+  const [images, setImages] = useState<IFile[]>([]);
   const [files, setFiles] = useState<File[]>([]);
   const [previews, setPreviews] = useState<{ name: string; url?: string; text?: string }[]>([]);
 
@@ -32,25 +41,61 @@ function App() {
 
   const uploadFiles = async () => {
     if (files.length === 0) return alert("파일이 없습니다.");
-  
+
     const formData = new FormData();
     files.forEach((file) => {
       formData.append("files", file); // Nest multer field name
     });
-  
+
     const res = await fetch("http://localhost:3000/files", {
       method: "POST",
       body: formData,
     });
-  
+
     const result = await res.json();
     console.log("업로드 결과:", result);
     alert("업로드 완료!");
   };
 
+  const getAllImages = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/files", {
+        method: "GET",
+      });
+
+      if (!res.ok) {
+        throw new Error("서버 응답 오류");
+      }
+
+      const data = await res.json();
+      console.log("파일 목록", data);
+
+      setImages(data);
+    } catch (error) {
+      console.error("파일 조회 실패", error)
+    }
+  };
+
+  useEffect(() => {
+    getAllImages();
+  }, [])
+
   return (
     <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
       <h1>파일 업로드 테스트</h1>
+
+      <div>저장 완료된 이미지들</div>
+      <div>
+        {
+          images.map((img) => (
+            <img
+              src={img.url}
+              alt={img.name}
+              style={{ width: "200px", marginTop: "10px", borderRadius: "6px" }}
+            />
+          ))
+        }
+      </div>
 
       <button onClick={uploadFiles}>저장</button>
 
