eb5226ef86a7221769d4a6c2a012252d5ff449a8
jw <yunjwon94@gmail.com>
2025-11-21 13:17:54 +0900

    fix: vite env 못읽음

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-console.log('Loaded ENV:', process.env.VITE_PORT);
+export default defineConfig(({ mode }) => {
+  // .env 파일 로드
+  const env = loadEnv(mode, process.cwd(), '')
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  server: {
-    port: Number(process.env.VITE_PORT) || 5173
+  console.log("Loaded ENV:", env.VITE_PORT)
+
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
+      }),
+    ],
+    server: {
+      port: Number(env.VITE_PORT) || 5173,
+    },
   }
 })
